1c2dd3ac8e7d991b81775e5066323c557bf1181d
Sachin Chavan <sachin.chavan043@gmail.com>
2020-04-28 18:04:01 +0530

    New updates to the program

--- SparkHiveDemo/src/main/scala/com/tgt/spark/test/hive/Program3.scala
+++ SparkHiveDemo/src/main/scala/com/tgt/spark/test/hive/Program3.scala
@@ -20,8 +20,6 @@ object Program3 {
     System.out.println("INFO: ****************** Starting Connection HIVE ******************")
     sqlContext.sql("CREATE TABLE IF NOT EXISTS employee(id INT, name STRING, age INT) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'")
     sqlContext.sql("Show tables").show()
-    //sqlContext.sql("LOAD DATA LOCAL INPATH 'C:/Users/CSC/Desktop/employee.txt' INTO TABLE employee")
-    //val result = sqlContext.sql("FROM employee SELECT id, name, age")
-    //result.show()
+    
   }
 }
